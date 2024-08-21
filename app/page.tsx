add1a8f0f95d2c56f3bce9aae5a19d01ba5a1da4
ttd123 <97510310029@ispace.edu.vn>
2024-08-21 14:38:02 +0700

    duong

---
+++ app/page.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+
+import WebApp from "@twa-dev/sdk";
+import { useEffect, useState } from "react";
+
+interface UserData{
+  id: number;
+  first_name: string;
+  last_name?: string;
+  username?: string;
+  language_code: string;
+  is_premium?: boolean;
+}
+
+
+export default function Home() {
+  const[userdata, setUserData] = useState<UserData | null>(null)
+
+  useEffect(() => {
+    if (WebApp.initDataUnsafe.user){
+      setUserData(WebApp.initDataUnsafe.user as UserData)
+    }
+    
+
+  }, [])
+
+  return (
+    <main className="p-4">
+      {
+        userdata ?
+        (
+          <>
+          <h1 className="text-2xl font-bold mb-4">User Data</h1>
+          <ul>
+            <li>ID: {userdata.id}</li>
+            <li>First Name: {userdata.first_name}</li>
+            <li>Last Name: {userdata.last_name}</li>
+            <li>Username: {userdata.username}</li>
+            <li>language Code: {userdata.language_code}</li>
+            <li>Is Premium: {userdata.is_premium ? 'Yes' : 'No'}</li>
+          </ul>
+          </>
+        ) :
+        (
+          <div>Loading...</div>
+        )
+      }
+
+    </main>
+
+  );
+}
